6464b0b526f1fbea4f6c20b40a3dbc77b97d877c
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-07-28 20:07:51 +0530

    index.js file completed

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const {Connect} = require("./db.js/connect.js");
+const userRouter = require("./routes/user.js")
 const cors = require("cors");
-const rootRouter = require("./routes/account");
-
-const app = express();
-const PORT = 8000;
-
+const accountRouter = require("./routes/account.js")
+const app = express(); 
 app.use(cors());
 app.use(express.json());
-
-
-app.use("/api/v1", rootRouter);
-app.listen(PORT, () => {
-    console.log(`I am running on PORT ${PORT}`);
+app.use(express.urlencoded({
+    extended : true, 
+    limit : "16kb"
+}));
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/account",accountRouter);
+app.listen(3000,async()=>
+{
+    try{
+        Connect();
+    }
+    catch(err)
+    {
+        console.log("error in connecting DB"); 
+    }
 })
